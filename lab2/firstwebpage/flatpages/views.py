e9eac89debea4b88044fa2404adc0b67bdd0eb4a
paleale <dovarij@gmail.com>
2020-06-26 21:48:22 +0300

    lab2 add tasks

--- lab2/firstwebpage/flatpages/views.py
+++ lab2/firstwebpage/flatpages/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 from django import template
 
 def home(request):
-  return render(request, 'templates/index.html')
+  return render(request, 'templates/static_handler.html')
