beedc6c0f9259e5d3c747c8dad5b227e870d908c
paleale <dovarij@gmail.com>
2020-06-26 20:44:38 +0300

    add lab2 code

---
+++ lab2/firstwebpage/flatpages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django import template
+
+def home(request):
+  return render(request, 'templates/index.html')
